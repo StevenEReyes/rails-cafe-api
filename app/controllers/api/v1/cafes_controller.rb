5a8076c7df3332ce9d4c14459614fdad5af457a8
Steven Reyes <steven.reyes88@gmail.com>
2024-09-05 16:07:48 +0900

    search added

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::CafesController < ApplicationController
   def index
-    @cafes = Cafe.all
+    if params[:title].present?
+      @cafes = Cafe.where("title ILIKE ?", "%#{params[:title]}%")
+    else
+      @cafes = Cafe.all
+    end
     render json: @cafes
   end
 end
