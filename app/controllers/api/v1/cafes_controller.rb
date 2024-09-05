78c2a8cd7a1368aa434a573f8217c08dcdd9cd71
Steven Reyes <steven.reyes88@gmail.com>
2024-09-05 16:20:15 +0900

    create added with error returns if missing data

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -5,6 +5,21 @@ class Api::V1::CafesController < ApplicationController
     else
       @cafes = Cafe.all
     end
-    render json: @cafes
+    render json: @cafes.order(created_at: :desc)
+  end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      render json: @cafe, status: :created
+    else
+      render json: { errors: @cafe.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
   end
 end
